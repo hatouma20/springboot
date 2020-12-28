fac0471686f77e332ccf5401a15c60361459b578
hatem <souki.hatem@gmail.com>
2020-12-28 12:25:32 +0100

    hello

---
+++ src/main/java/com/example/testing/dto/ProductDto.java
@@ -0,0 +1,20 @@
+package com.example.testing.dto;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+
+    @JsonBackReference
+    private List<DealerDto> dealers = new ArrayList<>();
+}
