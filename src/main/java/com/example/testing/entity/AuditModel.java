b9898059cfb48253edbdcecfa6cf993fec51b52d
hatem <souki.hatem@gmail.com>
2020-12-28 12:13:34 +0100

    hello

--- src/main/java/com/example/testing/entity/AuditModel.java
+++ src/main/java/com/example/testing/entity/AuditModel.java
@@ -1,4 +1,21 @@
 package com.example.testing.entity;
 
-public class AuditModel {
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AuditModel implements Serializable {
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt = Calendar.getInstance().getTime();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private Date updatedAt = Calendar.getInstance().getTime();
 }
